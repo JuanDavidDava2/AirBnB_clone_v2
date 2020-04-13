12ea710473cca8f597a6e0182ce754b3f6ee2dc3
JuanDavidDava2 <juandm417@gmail.com>
2020-04-13 03:43:23 +0000

    fix task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,43 @@
-#/usr/bin/env bash
-#Script to set up web servers for web_static deployment
+#!/usr/bin/env bash
+# This script configure a server with some specifications
 
+
+# Setup file directory system for webstatic files
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+
+sudo echo "<h1>This is my code<h1>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+# Standar 404 message
+sudo mkdir -p /var/www/html/
+sudo echo "Holberton School for the win!" | sudo tee /var/www/html/index.html
+
+# Install nginx web server
 sudo apt-get update
-sudo apt-get -y install nginx
+sudo apt-get install -y nginx
 
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
-
-sudo echo "this is my test" | sudo tee /data/web_static/releases/test/index.html
-
-echo "server {
-    listen 80;
+printf %s "server {
+    listen 80 default_server;
     listen [::]:80 default_server;
-    root   /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-    error_page 404 /404.html;
-    location = /404.html {
-        internal;
-    }
     add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
     location /hbnb_static {
         alias /data/web_static/current;
-			    }
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=DHITmcKUGik;
+    }
+    error_page 404 /404.html;
+    location /404 {
+        root /var/www/html;
+        internal;
+    }
 }" > /etc/nginx/sites-available/default
 
 sudo service nginx restart

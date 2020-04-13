8f820593a98692094af7176735a78e3c0d631c8c
JuanDavidDava2 <juandm417@gmail.com>
2020-04-13 03:56:59 +0000

    task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""generates .tgz archive """
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    "generates .tgz archive"
+    local("mkdir -p versions")
+    timeNow = datetime.now()
+    tgz = "versions/web_static_"
+    tgz += "{}{}{}".format(timeNow.year, timeNow.month, timeNow.day)
+    tgz += "{}{}{}".format(timeNow.hour, timeNow.minute, timeNow.second)
+    tgz += ".tgz"
+    result = "tar -cvzf "
+    result += tgz
+    result += " web_static"
+    if local(result) == 1:
+        return None
+return tgz

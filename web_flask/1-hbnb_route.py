fa1478a09b2c2a76d16b4e0f7cc7fb828edf7c57
JuanDavidDava2 <juandm417@gmail.com>
2020-04-22 18:57:39 +0000

    fix pep8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,9 +4,11 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_world():
     return "Hello HBNB!"
+
 
 @app.route('/hbnb')
 def hbnb():
